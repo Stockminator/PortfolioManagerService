ef2e0034affdec396d2ee44dadfde424ee2c7e85
attilapalfi92 <apalfi@cloudera.com>
2021-03-18 17:15:16 +0100

    first commit

---
+++ src/main/kotlin/com/stockminator/portfoliomanagerservice/PortfolioManagerServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.stockminator.portfoliomanagerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioManagerServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioManagerServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/stockminator/portfoliomanagerservice/PortfolioManagerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stockminator.portfoliomanagerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioManagerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
